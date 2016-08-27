ca53e537d88cbcdc398dc438c40548ac7829142b
Dunbarry <RyanADunbar@gmail.com>
2016-08-26 21:13:54 -0600

    targeting and turn mgt set.

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
 console.log('Drone Online');
 var targetCell="";
+var turn="Player"
+var safety="OFF"
+
+//Turn Mgt~~~~~~~~~~~~~~~~~~~~~
+function safetySwap(){            //Safety Controls
+  if(turn==="Player"){
+    safety="OFF"
+  }
+  else if(turn==="OP"){
+    safety="ON";
+  }
+  indicators();
+  console.log(safety)
+}
+
+function turnSwap(){          //Turn Controls
+  if(turn==="Player"){
+    turn="OP";
+    console.log("Wait for it...")
+    setTimeout(turnSwap,3000)
+  }
+  else if(turn==="OP"){
+    turn="Player"
+  }
+  safetySwap();
+  console.log(safety);
+  console.log(turn);
+}
+
+// function turnState(){
+//   return turn;
+// }
+
+// function safetyState(){
+//   return safety;
+// }
+
 //Fire Control~~~~~~~~~~~~~~~~
+function fireClear(){
+  $('#'+targetCell).removeClass('impact');
+  turnSwap();
+}
+
+function fire(){
+  $('#'+targetCell).addClass('impact');
+  setTimeout(fireClear,5000)
+}
+
 $('#bigRed').click(function(){
   targetCell=($('#degree').val()+"x"+$('#rotation').val())
-  $('#'+targetCell).css('background-color','red');
+  fire();
 })
 
 
+//UI Shiznit~~~~~~~~~~~~~~~~~~~~
+function indicators(){
+  if(safety==="OFF"){
+    $('#bigRed').addClass('impact');
+  }
+  else if(safety==="ON"){
+    $('#bigRed').removeClass('impact');
+  }
+}
 
+//ON load~~~~~~~~~~~~~~~~~~~~~~~
 $(document).ready(function(){
   var row=8
   while(row>0){
